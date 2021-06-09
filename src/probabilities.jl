311f063166ed552329881e7ad8477da2b7315613
mudssky <mudssky@gmail.com>
2021-06-09 14:00:39 +0800

    ✨ feat:  Permutation Number
    A function to calculate Permutation Number

--- src/probabilities.jl
+++ src/probabilities.jl
@@ -1,20 +1,21 @@
+using Base:Integer
 
-# factorial()
 """
-计算排列数 A_n^m
+计算排列数 A_n^m,输入需要是整数类型，返回值是BigInt类型，因为排列数很容易变成大数
 """
-function A(m, n)
-    if n < m
-        return -1
-    
-    elseif (n == m)
-        return factorial(n)
-    
-    else
-        if min(m, n) > 20
-        return factorial(big(n)) / factorial(big(n - m)) 
-        else
-            return factorial(n) / factorial(n - m)
+function A(m::Integer, n::Integer)
+    # basic param check
+    if n == 0 || m == 0
+        throw(ArgumentError("n or m should not be zero or negative"))
+    elseif n < m 
+        throw(ArgumentError("n should be lager than m"))
+            end
+
+    function A_inner(m, n)
+        if (m <= 1)
+            return n
         end
-        end
+        return A_inner(m - 1, n - 1) * n
+    end
+    return A_inner(BigInt(m), BigInt(n))
 end
